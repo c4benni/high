6c1a89b5d92fb580870472d87bc5aeef0966e86c
Chidi Benedict <c4benni@gmail.com>
2022-01-14 23:12:49 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import Form from './components/Form/Form';
+import Button from './components/Inputs/Button/Button';
+import TextField from './components/Inputs/TextField/TextField';
+import { AppLogo } from './components/Logo/appLogo';
+import Breakpoint from './utils/breakpoints';
 
 function App() {
+  useEffect(() => {
+    window.Breakpoint = Breakpoint;
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        New app
+    <div className="App grid items-start">
+      <header className="App-header ">
+        <AppLogo />
+
+        <Form
+          name='sign-in'
+          submitText='Sign in'
+        >
+          <TextField
+            label='Username'
+          />
+
+          <TextField
+            type='password'
+            label='Password'
+          />
+        </Form>
+
+        <Button tag='span' primary>
+          Hello Button
+        </Button>
       </header>
     </div>
   );
