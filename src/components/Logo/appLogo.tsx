bc4b53932eb99c20544f4f7095efaa75b3836c41
Chidi Benedict <c4benni@gmail.com>
2022-01-14 23:12:28 +0100

    Create appLogo.tsx

---
+++ src/components/Logo/appLogo.tsx
@@ -0,0 +1,7 @@
+import { SVGProps } from "react";
+
+export function AppLogo(props: SVGProps<SVGSVGElement>) {
+    return (
+        <svg width="1em" height="1em" viewBox="0 0 256 256" {...props}><path d="M132 24A100.2 100.2 0 0 0 32 124v84.3A15.7 15.7 0 0 0 47.7 224H132a100 100 0 0 0 0-200zm28 128h-60a8 8 0 0 1 0-16h60a8 8 0 0 1 0 16zm0-32h-60a8 8 0 0 1 0-16h60a8 8 0 0 1 0 16z" fill="currentColor"></path></svg>
+    )
+}
