50e312038a516105df53148ac68e49a35c0217ec
Chidi Benedict <c4benni@gmail.com>
2022-01-15 21:50:14 +0100

    Update Button.tsx

--- src/components/Inputs/Button/Button.tsx
+++ src/components/Inputs/Button/Button.tsx
@@ -22,19 +22,28 @@ function Button(props: ButtonProps) {
 
     const tag = props.tag || 'button'
 
+    const events = {} as { [key: string]: any };
+
+    for (const key in props) {
+        if (/on[A-Z]/.test(key)) {
+            events[key] = props[key]
+        }
+    }
+
     return (
         React.createElement(tag, {
-            itemProp: props,
+            ...events,
             tabIndex:
                 props.disabled ? '-1' : !/button|a/.test(tag) ? '0' : undefined,
             disabled: props.disabled,
             className: [
-                'Button',
+                'Button fill-before',
                 props.size,
-                !props.link ?
-                    `link underline-effect${props.icon ? ' icon' : ''
-                    }${props.primary ? ' primary' : ' no-bg'}${props.overlay ? 'border-[0.75px] border-white border-opacity-10 bg-white bg-opacity-10 hover:bg-opacity-20' : ''}${props.hidePlainFocus ? ' hide-plain-focus' : ''}`
-                    : `filled${props.primary ? ' primary-text' : ''}`,
+                props.link ? `link underline-effect${props.primary ? 'primary-text' : ''}` : '',
+                props.primary ? 'primary' : '',
+                props.overlay ? 'border-[0.75px] border-white border-opacity-10 bg-white bg-opacity-10 hover:bg-opacity-20' : '',
+                props.hidePlainFocus ? 'hide-plain-focus' : '',
+                !props.link && (props.primary || props.secondary) ? 'filled' : 'no-bg',
                 props.block ? 'grid w-full' : '',
                 props.className
             ].filter(Boolean).join(' '),
