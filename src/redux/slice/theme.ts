d949aa3cd75943c9b8981bcabce7776bc1683d82
Chidi Benedict <c4benni@gmail.com>
2022-01-15 21:51:50 +0100

    create theme slice

---
+++ src/redux/slice/theme.ts
@@ -0,0 +1,45 @@
+import { createSlice, PayloadAction, Slice } from "@reduxjs/toolkit";
+
+export type ThemeOption = "light" | "dark" | "";
+
+export type ThemeSlice = {
+  is: ThemeOption;
+  light: boolean;
+  dark: boolean;
+};
+
+const initialState: ThemeSlice = {
+  is: "",
+  light: true,
+  dark: true,
+};
+
+const themeSlice: Slice<ThemeSlice> = createSlice({
+  name: "theme",
+  initialState,
+  reducers: {
+    setTheme: (state, action: PayloadAction<string>) => {
+      if (/light|dark/.test(action.payload)) {
+        state.is = action.payload as ThemeOption;
+      } else state.is = "dark";
+
+      state.dark = state.is !== "light";
+      state.light = state.is === "light";
+
+      // if (process.client) {
+      const isLight = state.light;
+
+      const currentTheme = isLight ? "dark" : "light";
+      const replaceWith = isLight ? "light" : "dark";
+
+      const html = document.documentElement;
+      html.classList.add(currentTheme);
+      html.classList.replace(currentTheme, replaceWith);
+      // }
+    },
+  },
+});
+
+export const { setTheme } = themeSlice.actions;
+
+export default themeSlice.reducer;
