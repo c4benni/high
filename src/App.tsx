5208e4d88e8694797d440d6d68ab3c96782a9c66
Chidi Benedict <c4benni@gmail.com>
2022-01-17 07:59:56 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import { Root } from './view/Root';
 
+import { CloudinaryContext } from 'cloudinary-react';
 
 function App() {
   return (
-    <Provider store={store}>
-      <Root />
-    </Provider>
+    <CloudinaryContext cloudName="c4benn">      
+      <Provider store={store}>
+        <Root />
+      </Provider>
+    </CloudinaryContext>
   );
 }
 
