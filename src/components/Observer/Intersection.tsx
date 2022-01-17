aecef4343dc2fce2bd1ec4113620699c1f5c6da8
Chidi Benedict <c4benni@gmail.com>
2022-01-17 08:00:32 +0100

    Update Intersection.tsx

--- src/components/Observer/Intersection.tsx
+++ src/components/Observer/Intersection.tsx
@@ -1,11 +1,60 @@
-import React from 'react'
+import { useEffect, useRef, useState } from "react";
 
-function Intersection() {
-    return (
-        <>
+export type IntersectionConfig = {
+    threshold?: number | number[];
+    rootMargin?: string;
+    root?: HTMLElement | null;
+};
 
-        </>
-    )
-}
+const defaultConfig: IntersectionConfig = {
+    threshold: 0,
+    rootMargin: "0px 0px 0px 0px",
+    root: null,
+};
 
-export default Intersection
+type PropsType = {
+    once?: boolean;
+    options?: IntersectionConfig;
+    disabled?: boolean;
+    children: Function;
+    onceIntercepted?: Function;
+    onUpdate?: Function;
+};
+
+export type Ref<El> = React.LegacyRef<El> | undefined;
+
+export type Entry = IntersectionObserverEntry
+
+const Intersection = (props: PropsType) => {
+    const intersectionRef = useRef(null);
+
+    const { options = defaultConfig } = props;
+
+    const [entry, $entry] = useState<IntersectionObserverEntry | null>(null);
+
+    const callback = (entries: IntersectionObserverEntry[]) => {
+        const [entry] = entries;
+
+        $entry(entry);
+    };
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(callback, options);
+        let target: HTMLElement;
+
+        if (intersectionRef.current) {
+            target = intersectionRef.current;
+            observer.observe(target);
+        }
+
+        return () => {
+            if (target) {
+                observer.unobserve(target);
+            }
+        };
+    }, [intersectionRef, options]);
+
+    return props.children(intersectionRef, entry);
+};
+
+export default Intersection;
