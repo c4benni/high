b4f2e6ef47bfc123efc07a21c362255edc6f3eac
Chidi Benedict <c4benni@gmail.com>
2022-01-17 08:01:08 +0100

    Update Onboard.tsx

--- src/view/Onboard.tsx
+++ src/view/Onboard.tsx
@@ -1,15 +1,39 @@
 import React from 'react'
+import Img from '../components/Display/Img'
 import SignIn from '../components/Onboard/SignIn'
+import { className } from '../components/utils/main';
+
+import useBreakpoint from '../hooks/breakpoint'
 
 function Onboard() {
+    const [breakpoint] = useBreakpoint();
+
     return (
         <div
-            className='grid grid-flow-col h-full items-start isolate'
+            className={
+                className([
+                    'grid grid-flow-col h-full items-start isolate md:grid-cols-[auto,1fr]',
+                    {
+                        dark: breakpoint.isMobile
+                    }
+                ])
+            }
         >
-            <SignIn />
+            <SignIn className='min-w-full md:min-w-[550px] my-12' />
 
-            <div className='bg-success-600 flex-1 md:col-span-3 lg:col-span-2 xl:col-span-5 h-full fixed md:relative top-0 md:top-auto w-full -z-10 md:z-auto'>
-
+            <div className={
+                className([
+                    'flex-1 md:col-span-3 lg:col-span-2 xl:col-span-5 h-full fixed md:sticky top-0 w-full -z-10 md:z-auto overflow-hidden max-h-screen xl:rounded-r-lg',
+                    {
+                        'fill-after after:z-10 isolate after:bg-black after:bg-opacity-[0.85]': breakpoint.isMobile
+                    }
+                ])
+            }>
+                <Img
+                    alt='splash image'
+                    publicId="highChat/splash/daniel-korpai-r73OFSry5AI-unsplash"
+                    className='h-full object-cover object-top w-full'
+                />
             </div>
         </div>
     )
