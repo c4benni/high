4021c08cc9f8e85fc01a7c3ec6ddfc289f1a031e
Chidi Benedict <c4benni@gmail.com>
2022-01-18 22:04:54 +0100

    Create Alert.tsx

---
+++ src/components/Icon/Generic/Alert.tsx
@@ -0,0 +1,7 @@
+import { SVGProps } from "react";
+
+export function AlertIcon(props: SVGProps<SVGSVGElement>) {
+    return (
+        <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}><path d="M13 14h-2V9h2m0 9h-2v-2h2M1 21h22L12 2L1 21z" fill="currentColor"></path></svg>
+    )
+}
