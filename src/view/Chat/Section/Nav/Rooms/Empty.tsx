24c1c3c19d95b7f40b23f8e0ff646c0b6f5ac7a6
Chidi Benedict <c4benni@gmail.com>
2022-01-19 18:28:58 +0100

    Create Empty.tsx

---
+++ src/view/Chat/Section/Nav/Rooms/Empty.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { ArrowRight } from '../../../../../components/Icon/Generic/ArrowRight';
+import { PlusIcon } from '../../../../../components/Icon/Generic/Plus';
+import IconWrapper from '../../../../../components/Icon/Logo/IconWrapper';
+import Button from '../../../../../components/Inputs/Button/Button';
+
+function Empty() {
+    return <div
+        className='grid justify-center pt-8 pb-6 px-3'
+    >
+        <p
+            className='text-caption mb-6'
+        >
+            You don't belong to any room
+        </p>
+
+        <Button
+            primary
+            className={'h-[42px] mb-4'}
+        >
+            <IconWrapper>
+                <PlusIcon />
+            </IconWrapper>
+
+            Create a room
+        </Button>
+
+        <Button
+            title='Explore rooms'
+            link
+            primary
+            append={
+                <IconWrapper>
+                    <ArrowRight />
+                </IconWrapper>
+            }
+            className='link-arrow arrow-right justify-self-center'
+        />
+    </div>;
+}
+
+export default Empty;
