5609d0d9135cf811d9c2ca0c405b51ac06b23f31
Chidi Benedict <c4benni@gmail.com>
2022-01-19 15:24:13 +0100

    create navIcons

---
+++ src/components/Icon/Generic/ChatEmpty.tsx
@@ -0,0 +1,7 @@
+import { SVGProps } from "react";
+
+export function ChatEmpty(props: SVGProps<SVGSVGElement>) {
+    return (
+        <svg width="1em" height="1em" viewBox="0 0 256 256" {...props}><path d="M232 124a100.2 100.2 0 0 1-100 100H47.7A15.7 15.7 0 0 1 32 208.3V124a100 100 0 0 1 200 0z" fill="currentColor"></path></svg>
+    )
+}
---
+++ src/components/Icon/Generic/ChatTyping.tsx
@@ -0,0 +1,7 @@
+import { SVGProps } from "react";
+
+export function ChatTyping(props: SVGProps<SVGSVGElement>) {
+    return (
+        <svg width="1em" height="1em" viewBox="0 0 256 256" {...props}><path d="M132 24A100.2 100.2 0 0 0 32 124v84.3A15.7 15.7 0 0 0 47.7 224H132a100 100 0 0 0 0-200zM84 140a12 12 0 1 1 12-12a12 12 0 0 1-12 12zm48 0a12 12 0 1 1 12-12a12 12 0 0 1-12 12zm48 0a12 12 0 1 1 12-12a12 12 0 0 1-12 12z" fill="currentColor"></path></svg>
+    )
+}
