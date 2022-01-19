974d69b04f53cd377b951122623240ec23b256d9
Chidi Benedict <c4benni@gmail.com>
2022-01-19 18:27:05 +0100

    create icons

---
+++ src/components/Icon/Generic/ArrowRight.tsx
@@ -0,0 +1,7 @@
+import { SVGProps } from "react";
+
+export function ArrowRight(props: SVGProps<SVGSVGElement>) {
+    return (
+        <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}><g fill="none"><path d="M14 5l7 7m0 0l-7 7m7-7H3" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path></g></svg>
+    )
+}
---
+++ src/components/Icon/Generic/Plus.tsx
@@ -0,0 +1,7 @@
+import { SVGProps } from "react";
+
+export function PlusIcon(props: SVGProps<SVGSVGElement>) {
+    return (
+        <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}><path d="M18 12.998h-5v5a1 1 0 0 1-2 0v-5H6a1 1 0 0 1 0-2h5v-5a1 1 0 0 1 2 0v5h5a1 1 0 0 1 0 2z" fill="currentColor"></path></svg>
+    )
+}
