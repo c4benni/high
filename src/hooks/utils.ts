2816cd918d579b03323f91d45325ea5a814b8c29
Chidi Benedict <c4benni@gmail.com>
2022-01-20 08:59:07 +0100

    added useChatAside hook

--- src/hooks/utils.ts
+++ src/hooks/utils.ts
@@ -1,8 +1,21 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useLocation } from "react-router-dom";
+import { AsideSlice, setAside } from "../redux/slice/appState";
+import { RootState } from "../redux/store";
 
 export function useQuery() {
   const { search } = useLocation();
 
   return React.useMemo(() => new URLSearchParams(search), [search]);
+}
+
+export function useChatAside() {
+  const dispatch = useDispatch();
+
+  const { chatAside } = useSelector((state: RootState) => state.appStateSlice);
+
+  const toggleSidebar = (newState: AsideSlice) => dispatch(setAside(newState));
+
+  return [chatAside, toggleSidebar] as const;
 }
