10213f7f9ff9fd6bd0270619564af654e1e44877
Chidi Benedict <c4benni@gmail.com>
2022-01-20 09:01:09 +0100

    Update Index.tsx

--- src/view/Chat/Section/Index.tsx
+++ src/view/Chat/Section/Index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { className } from '../../../components/utils/main'
 import useBreakpoint from '../../../hooks/breakpoint'
+import { useChatAside } from '../../../hooks/utils'
 import Aside from './Aside/Index'
 import Header from './Header'
 import Main from './Main/Index'
@@ -9,13 +10,19 @@ import Nav from './Nav/Index'
 function Section() {
     const [breakpoint] = useBreakpoint();
 
+    const [showAside] = useChatAside()
+
     return (
         <section
             className={
                 className([
                     'bg-white dark:bg-true-gray-800 grid relative md:ml-1',
                     !breakpoint.isMobile ? [
-                        'grid-flow-col grid-cols-[240px,1fr,240px] h-[calc(100%-0px)] self-end rounded-tl-lg overflow-hidden'
+                        'grid-flow-col h-[calc(100%-0px)] self-end rounded-tl-lg overflow-hidden',
+                        {
+                            'grid-cols-[240px,1fr,240px]': showAside.visible,
+                            'grid-cols-[240px,1fr]': !showAside.visible
+                        }
                     ] : []
                 ])
             }
@@ -26,7 +33,9 @@ function Section() {
 
             <Main />
 
-            <Aside />
+            {
+                showAside.visible ? <Aside /> : null
+            }
 
         </section>
     )
