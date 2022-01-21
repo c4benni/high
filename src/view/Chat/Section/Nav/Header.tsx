ec03e0935dcb42ab12c5322987bbe47ab042f488
Chidi Benedict <c4benni@gmail.com>
2022-01-21 23:23:37 +0100

    update navHeader

--- src/view/Chat/Section/Nav/Header.tsx
+++ src/view/Chat/Section/Nav/Header.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import Form from '../../../../components/Form/Form';
-import { SearchIcon } from '../../../../components/Icon/Generic/Search';
-import IconWrapper from '../../../../components/Icon/Logo/IconWrapper';
-import TextField from '../../../../components/Inputs/TextField/TextField';
+import SearchField from '../../../../components/Inputs/TextField/SearchField';
 import { className } from '../../../../components/utils/main';
 
 type Props = {
@@ -20,39 +17,15 @@ function Header(props: Props) {
             ])
         }
     >
-        <Form
-            name='search-MAKE_REACTIVE!!!!!!!!'
-            className='mx-auto w-full'
-        >
-            <TextField
-                label='Search'
-                // make this reactive too!!!!
-                placeholder='Search rooms'
-                value={props.searchInput}
-                onModel={props.onSearchInput}
-                hideLabel
-                disabled={disableInput}
-                className='text-sm'
-                wrapperClassName={[
-                    'h-[40px] md:h-[28px]'
-                ]}
-                inputClassName={'pl-[32px] md:pl-[24px] border-none bg-gray-200 dark:bg-gray-900'}
-                append={
-                    <IconWrapper
-                        className={
-                            className([
-                                'absolute w-[32px] md:w-[24px] justify-center flex pointer-events-none left-0',
-                                {
-                                    'opacity-70': !disableInput
-                                }
-                            ])
-                        }
-                    >
-                        <SearchIcon />
-                    </IconWrapper>
-                }
-            />
-        </Form>
+        <SearchField
+            formName='search-MAKE_REACTIVE!!!!!!!!'
+            label='Search'
+            // make this reactive too!!!!
+            placeholder='Search rooms'
+            value={props.searchInput}
+            onModel={props.onSearchInput}
+            disabled={disableInput}
+        />
     </header>;
 }
 
