aa4ebb3999f761f4660b7847bd2cb48269004571
Chidi Benedict <c4benni@gmail.com>
2022-01-21 23:23:11 +0100

    Update Heading.tsx

--- src/view/Chat/Section/Header/Heading.tsx
+++ src/view/Chat/Section/Header/Heading.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import InfoDialog from '../../../../components/Display/Overlay/Dialog/InfoDialog';
 import { HashIcon } from '../../../../components/Icon/Generic/Hash';
 import { HorizontalDots } from '../../../../components/Icon/Generic/HorizontalDots';
 import { InviteIcon } from '../../../../components/Icon/Generic/Invite';
 import IconWrapper from '../../../../components/Icon/Logo/IconWrapper';
 import Button from '../../../../components/Inputs/Button/Button';
 
+const subtitle = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse itaque aspernatur aperiam voluptatum, nihil recusandae cum atque natus. Tempore, hic ipsam. Obcaecati ex at reiciendis quis pariatur aliquam libero aperiam?'
+
+const title = 'Room name'
+
 function Heading() {
+    const [dialog, setDialog] = useState(false);
+
     return <div
         className='grid grid-flow-col grid-cols-[auto,auto,1fr,auto] items-center pl-3 pr-1'
     >
@@ -14,18 +21,29 @@ function Heading() {
                 <HashIcon />
             </IconWrapper>
 
-            Group name
+            {title}
         </h1>
 
         <span
             className='mx-3 h-[60%] bg-gray-300 dark:bg-gray-700 opacity-80 w-[1px]'
         />
 
-        <h2
-            className='truncate cursor-pointer text-gray-600 dark:text-gray-300 hover:opacity-80 transition-opacity active:opacity-70 justify-self-start max-w-full'
-        >
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse itaque aspernatur aperiam voluptatum, nihil recusandae cum atque natus. Tempore, hic ipsam. Obcaecati ex at reiciendis quis pariatur aliquam libero aperiam?
-        </h2>
+        <>
+            <h2
+                className='truncate cursor-pointer text-gray-600 dark:text-gray-300 hover:opacity-80 transition-opacity active:opacity-70 justify-self-start max-w-full text-sm'
+                onClick={() => setDialog(!dialog)}
+            >
+                {subtitle}
+            </h2>
+
+            <InfoDialog
+                open={dialog}
+                onToggle={setDialog}
+                title={title}
+            >
+                {subtitle}
+            </InfoDialog>
+        </>
 
         <div
             className='ml-3'
