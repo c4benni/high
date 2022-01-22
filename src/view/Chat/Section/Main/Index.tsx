0458caa16bb733dd92582caa09940465beee3a31
Chidi Benedict <c4benni@gmail.com>
2022-01-22 22:07:15 +0100

    update mainChatSection

--- src/view/Chat/Section/Main/Index.tsx
+++ src/view/Chat/Section/Main/Index.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import Room from './Room';
+import TextArea from './TextArea';
 
 function Main() {
     return <main
-        className='select-auto'
-    ></main>;
+        className='select-auto grid grid-rows-[1fr,auto] pt-[48px] max-h-screen'
+    >
+
+        <Room />
+
+        <TextArea />
+    </main>;
 }
 
 export default Main;
