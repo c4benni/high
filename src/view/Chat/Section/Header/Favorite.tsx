2fcc244e5156845c8534bd80ef6abe301ef25a61
Chidi Benedict <c4benni@gmail.com>
2022-01-22 22:06:20 +0100

    update favourite
    added tooltip

--- src/view/Chat/Section/Header/Favorite.tsx
+++ src/view/Chat/Section/Header/Favorite.tsx
@@ -1,23 +1,30 @@
 import React, { useState } from 'react';
+import Tooltip from '../../../../components/Display/Overlay/Tooltip';
 import { StarIcon } from '../../../../components/Icon/Generic/Star';
 import Button from '../../../../components/Inputs/Button/Button';
 
 function Favorite() {
     const [state, setState] = useState(false);
 
-    return <Button
-        icon
-        htmlTitle='More'
-        className={{
-            'text-warning-500': state,
-            'text-gray-600 dark:text-gray-300': !state
+    return <Tooltip
+        title='Add to Favorite'
+        activator={({ ref, events }) => {
+            return <Button
+                ref={ref}
+                {...events}
+                icon
+                className={{
+                    'text-warning-500': state,
+                    'text-gray-600 dark:text-gray-300': !state
+                }}
+                onClick={() => {
+                    setState(!state)
+                }}
+                iconSlot={
+                    <StarIcon />
+                }
+            />
         }}
-        onClick={() => {
-            setState(!state)
-        }}
-        iconSlot={
-            <StarIcon />
-        }
     />
 }
 
