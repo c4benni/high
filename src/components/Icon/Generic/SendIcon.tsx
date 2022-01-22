ad301acfcf522b7fd17906ad7fcfdff03dbfb447
Chidi Benedict <c4benni@gmail.com>
2022-01-22 22:04:00 +0100

    Create SendIcon.tsx

---
+++ src/components/Icon/Generic/SendIcon.tsx
@@ -0,0 +1,7 @@
+import { SVGProps } from "react";
+
+export function SendIcon(props: SVGProps<SVGSVGElement>) {
+    return (
+        <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}><g fill="none"><path d="M12.815 12.197l-7.532 1.255a.5.5 0 0 0-.386.318L2.3 20.728c-.248.64.421 1.25 1.035.942l18-9a.75.75 0 0 0 0-1.341l-18-9c-.614-.307-1.283.303-1.035.942l2.598 6.958a.5.5 0 0 0 .386.318l7.532 1.255a.2.2 0 0 1 0 .395z" fill="currentColor"></path></g></svg>
+    )
+}
