dca370293b2ffd9dd51d36d6d312d52a6cc8134a
Chidi Benedict <c4benni@gmail.com>
2022-01-22 22:07:28 +0100

    create mainChatRoom

---
+++ src/view/Chat/Section/Main/Room/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Room() {
+    return <div className='overflow-y-auto max-h-full'>
+        <div className='h-screen'></div>
+    </div>;
+}
+
+export default Room;
