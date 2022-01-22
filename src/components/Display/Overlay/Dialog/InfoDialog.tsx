800e9612e6b63caa60031598c691d5a4e365f803
Chidi Benedict <c4benni@gmail.com>
2022-01-22 22:02:36 +0100

    Update InfoDialog.tsx

--- src/components/Display/Overlay/Dialog/InfoDialog.tsx
+++ src/components/Display/Overlay/Dialog/InfoDialog.tsx
@@ -1,8 +1,10 @@
-import { ReactNode } from 'react';
-import Dialog from '.';
+import { ReactNode, useMemo } from 'react';
+import { uid } from '../../../../utils/main';
 import { CloseIcon } from '../../../Icon/Generic/Close';
 import Button from '../../../Inputs/Button/Button';
 import { Slot } from '../../../utils/types';
+import Overlay from '../Overlay';
+import Tooltip from '../Tooltip';
 
 type Props = {
     open: boolean;
@@ -14,24 +16,38 @@ type Props = {
 function InfoDialog(props: Props) {
     const { open, onToggle, children, title } = props
 
-    return <Dialog
+    const id = useMemo(() => `infoDialog-${uid()}`, [])
+
+    return <Overlay
         open={open}
         onToggle={onToggle}
-        className={'rounded-md max-w-[95vw] sm:max-w-sm md:max-w-md min-h-[180px] md:min-h-[200px] bg-white dark:bg-gray-800 shadow-2xl'}
+        className={'rounded-md max-w-[92.5vw] sm:max-w-sm md:max-w-md min-h-[180px] md:min-h-[200px] bg-white dark:bg-gray-800 shadow-2xl dark:shadow-none dark:border default-border-color border-opacity-0 dark:border-opacity-40'}
+        dialogAttrs={{
+            'aria-labelledby': id
+        }}
     >
         <header
-            className='flex justify-between p-3 items-center border-b default-border-color dark:border-b-[0.75px]'
+            className='grid grid-flow-col grid-cols-[1fr,auto] justify-between p-3 items-center border-b default-border-color dark:border-b-[0.75px] gap-x-1'
         >
-            <h2 className='text-title text-xl font-bold'>
+            <h2
+                id={id}
+                className='text-title text-xl font-bold truncate'>
                 {title}
             </h2>
 
-            <Button
-                icon
-                iconSlot={
-                    <CloseIcon />
-                }
-                onClick={() => onToggle(false)}
+            <Tooltip
+                title='Close dialog'
+                activator={({ ref, events }) => {
+                    return <Button
+                        ref={ref}
+                        {...events}
+                        icon
+                        iconSlot={
+                            <CloseIcon />
+                        }
+                        onClick={() => onToggle(false)}
+                    />
+                }}
             />
         </header>
 
@@ -40,7 +56,7 @@ function InfoDialog(props: Props) {
         >
             {children}
         </div>
-    </Dialog>
+    </Overlay>
 }
 
 export default InfoDialog;
