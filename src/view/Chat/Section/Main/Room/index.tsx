8625ed51972f70fb8d2e257e55f87eb028756f41
Chidi Benedict <c4benni@gmail.com>
2022-01-23 16:21:23 +0100

    update mainRoom

--- src/view/Chat/Section/Main/Room/index.tsx
+++ src/view/Chat/Section/Main/Room/index.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import ChatBubble from '../../../../../components/Display/ChatBubble';
+import ChatBubbleWrapper from '../../../../../components/Display/ChatBubble/Wrapper';
+
+const message = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo a perspiciatis itaque pariatur? Porro itaque maiores aut qui, tempora consequuntur rem magni. Minus delectus ea illo optio, deserunt aperiam tenetur?'
 
 function Room() {
     return <div className='overflow-y-auto max-h-full'>
-        <div className='h-screen'></div>
+        <ChatBubbleWrapper>
+            <ChatBubble
+                role='receiver'
+                message={message}
+                isFirstMessage
+            />
+
+            <ChatBubble
+                role='sender'
+                message={message}
+            />
+            <ChatBubble
+                role='sender'
+                message={message}
+                subsequent
+            />
+        </ChatBubbleWrapper>
     </div>;
 }
 
