042a6f676228f210d3eab5be1d7a2b0b57a1df4e
Chidi Benedict <c4benni@gmail.com>
2022-01-25 17:41:17 +0100

    Update Reaction.tsx

--- src/components/Display/ChatBubble/Reaction.tsx
+++ src/components/Display/ChatBubble/Reaction.tsx
@@ -25,31 +25,33 @@ function Reaction(props: Props) {
             ])
         }
     >
-        <Tooltip
-            title={reaction.title}
-            activator={({ ref, events }) => {
-                // @ts-ignore
-                return <Button
-                    key={reaction.title}
-                    ref={ref}
-                    {...events}
-                    aria-label={reaction.title}
-                    disabled={!type}
-                    tabIndex={0}
-                    className={
-                        [
-                            'fade-appear self-center rounded-full p-[0.5px] min-w-[1.5rem] min-h-[1.5rem] h-[auto] transition-opacity md:hover:before:opacity-10 active:before:opacity-20 md:active:before:opacity-20 cursor-pointer fill-before before:transition-opacity before:opacity-0 before:bg-current relative before:-z-1 isolate',
-                            {
-                                'text-error-600 dark:text-error-500': type === 'love',
-                                'text-warning-600 dark:text-warning-400': type === 'exclaim'
-                            }
-                        ]
-                    }
-                >
-                    {reaction.emoji}
-                </Button>
-            }}
-        />
+        {
+            reaction.emoji ? <Tooltip
+                title={reaction.title}
+                activator={({ ref, events }) => {
+                    // @ts-ignore
+                    return <Button
+                        key={reaction.title}
+                        ref={ref}
+                        {...events}
+                        aria-label={reaction.title}
+                        disabled={!type}
+                        tabIndex={0}
+                        className={
+                            [
+                                'fade-appear self-center rounded-full p-[0.5px] min-w-[1.5rem] min-h-[1.5rem] h-[auto] transition-opacity md:hover:before:opacity-10 active:before:opacity-20 md:active:before:opacity-20 cursor-pointer fill-before before:transition-opacity before:opacity-0 before:bg-current relative before:-z-1 isolate',
+                                {
+                                    'text-error-600 dark:text-error-500': type === 'love',
+                                    'text-warning-600 dark:text-warning-400': type === 'exclaim'
+                                }
+                            ]
+                        }
+                    >
+                        {reaction.emoji}
+                    </Button>
+                }}
+            /> : null
+        }
     </div>
 }
 
